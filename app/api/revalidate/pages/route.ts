9c08c9f25381a07459f90244259013cafd5a4da4
Dan Sivewright <dan@wixels.com>
2024-02-08 12:38:10 +0200

    revalidate apis

---
+++ app/api/revalidate/pages/route.ts
@@ -0,0 +1,21 @@
+import { revalidateTag } from "next/cache"
+import { headers } from "next/headers"
+
+export async function POST(request: Request) {
+  const headersList = headers()
+
+  const secret = headersList.get("secret")
+  if (secret !== process.env.REVALIDATE_SECRET)
+    return new Response("Unauthorized", { status: 401 })
+  try {
+    revalidateTag("pages")
+  } catch (error) {
+    return new Response(`Webhook error: ${JSON.stringify(error as any)}`, {
+      status: 400,
+    })
+  }
+
+  return new Response("Success!", {
+    status: 200,
+  })
+}
---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,23 @@
+import { revalidateTag } from "next/cache"
+import { headers } from "next/headers"
+
+export async function POST(request: Request) {
+  const headersList = headers()
+  const body = await request.json()
+
+  const secret = headersList.get("secret")
+  if (secret !== process.env.REVALIDATE_SECRET)
+    return new Response("Unauthorized", { status: 401 })
+  try {
+    console.log("body::: ", body)
+    // revalidateTag("events")
+  } catch (error) {
+    return new Response(`Webhook error: ${JSON.stringify(error as any)}`, {
+      status: 400,
+    })
+  }
+
+  return new Response("Success!", {
+    status: 200,
+  })
+}
